39a828800c584b166ffa7f77726de90772fad8a6
lblonk <lblonk@users.noreply.github.com>
2020-01-25 22:06:44 +0100

    added button for new image

--- webapp/rawebapp/__init__.py
+++ webapp/rawebapp/__init__.py
@@ -1,5 +1,5 @@
-import os
-from flask import Flask, url_for, render_template
+import os,io
+from flask import Flask, url_for, render_template,send_file
 from .getimage import get_image
 from pathlib import Path
 
@@ -9,6 +9,7 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_KEY='dev',
     )
+
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -25,12 +26,24 @@ def create_app(test_config=None):
 
     @app.route('/')
     def index():
-        img = get_image()
-        dest_path = Path(app.root_path) /'static/newimage.bmp'
-
-        if not dest_path.parent.exists():
-            dest_path.parent.mkdir()
-        img.save(dest_path)
+        # img = get_image()
+        # dest_path = Path(app.root_path) /'static/newimage.bmp'
+        #
+        # if not dest_path.parent.exists():
+        #     dest_path.parent.mkdir()
+        # img.save(dest_path)
         return render_template('image.html')
+
+
+    @app.route('/image_file')
+    def image_file():
+        img = get_image()
+        output = io.BytesIO()
+        img.convert('RGBA').save(output, format='PNG')
+        output.seek(0, 0)
+        return send_file(output,mimetype='image/png', as_attachment=False)
+
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0  # crude solution to avoid browser caching generated images
+
 
     return app
